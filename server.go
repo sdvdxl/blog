67d2768f3a4f606a750fbccd638ce1c1f6240921
sdvdxl <sdvdxl@163.com>
2016-03-09 09:40:00 +0800

    init

---
+++ server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
