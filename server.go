6b348c5f37361924d056cc7fc2b9be0cdf253df7
sdvdxl <sdvdxl@163.com>
2016-03-09 13:11:21 +0800

    add webhook

--- server.go
+++ server.go
@@ -3,11 +3,21 @@ package main
 import (
 	"net/http"
 	"log"
+	"os/exec"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
+
+	//用于git的webhook，触发pull
+	http.HandleFunc("/_blog/_pull", func(writer http.ResponseWriter, request *http.Request) {
+		cmd := exec.Command("git","pull")
+		if err:=cmd.Start(); err!=nil {
+			log.Println("git pull error", err)
+		}
+	})
+
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
